bcc2d756e605af334f38bbe46eb66a0b779438a9
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-10-15 14:15:33 +0200

    commit4

--- technical-debt-areas.service.spec.ts
+++ technical-debt-areas.service.spec.ts
@@ -9,22 +9,67 @@ import { Environment } from "../babylon/environment";
 import { Model } from "../babylon/model";
 import { BabylonEngineService } from "./babylon-engine.service";
 import { TechnicalDebtAreasService } from "./technical-debt-areas.service";
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe("TechnicalDebtAreasService", () => {
   let babylonEngService: BabylonEngineService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([]),],
+      imports: [HttpClientTestingModule],
     });
 
-    babylonEngService = TestBed.inject(BabylonEngineService);
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        })
+    babylonEngService = TestBed.get(BabylonEngineService);
   });
 
   it("should be created", () => {
-    const service: TechnicalDebtAreasService = TestBed.inject(
+    const service: TechnicalDebtAreasService = TestBed.get(
       TechnicalDebtAreasService
     );
     expect(service).toBeTruthy();
+  });
+
+  it("it should return technicaldebtareas vertices", () => {
+    const service: TechnicalDebtAreasService = TestBed.get(
+      TechnicalDebtAreasService
+    );
+
+    spyOn(babylonEngService, "getMainModelVerticesData").and.returnValue([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      2,
+      29,
+      30,
+    ]);
+
+    expect(service.getTechnicalDebtVertices("testModelName").length).toBe(9);
   });
 });
